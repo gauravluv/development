a7cb12d9b3b334fd0712bff3103181a53f0c24f8
princegaurav <princegauravbbu@gmail.com>
2023-11-23 12:40:04 +0530

    learning

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 
-const form=document.querySelector("form");
+// const form=document.querySelector("form");
 
-form.addEventListener("submit",function(e){
-    e.preventDefault();
+// form.addEventListener("submit",function(e){
+//     e.preventDefault();
 
- const name=document.querySelector("#Name").value;
- const password=document.querySelector("#Password").value;
-
-
-    if(name==null || name==""){
-         alert("please enter name");
-    }
-
-    if(password=="" || password.length<6){
-        alert("please enter valid password");
-    }
+//  const name=document.querySelector("#Name").value;
+//  const password=document.querySelector("#Password").value;
 
 
-})
+//     if(name==null || name==""){
+//          alert("please enter name");
+//     }
+
+//     if(password=="" || password.length<6){
+//         alert("please enter valid password");
+//     }
+
+
+// })
+
+console.log("learning phase");
